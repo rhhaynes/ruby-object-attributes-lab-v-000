3db9e31a7880140511ec38f3a0c1859553fa3843
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-13 16:00:50 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    puts @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    puts @breed
+  end
+  
+end
